e786e9cb54d50c923798933c756a0485a72dbdda
Fernando Wesley Teixeira <fernando.teixeira@upflow.me>
2025-10-09 11:32:47 -0300

    Adiciona interrupção de tarefa;

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -59,6 +59,23 @@ export function MainForm() {
         })
     }
 
+    function handleInterruptTask() {
+        setState(prevState => {
+            return {
+                ...prevState,                
+                activeTask: null,                
+                secondsRemaining: 0,
+                formattedSecondsRemaining: '00:00',
+                tasks: prevState.tasks.map(task => {
+                    if (prevState.activeTask && prevState.activeTask.id === task.id) {
+                        return {...task, interruptDate: Date.now()};
+                    }
+                    return task;
+                })
+            }
+        })
+    }
+
     return (
         <form onSubmit={handlerCreateNewTask} className="form" action="">          
             <div className="formRow">
@@ -85,21 +102,26 @@ export function MainForm() {
             )}
             
             <div className="formRow">
-                {!state.activeTask ? (
+                {!state.activeTask && (
                     <DefaultButton 
                         type='submit' 
                         icon={<PlayCircleIcon />} 
                         color='green'
                         aria-label='Iniciar nova tarefa'
                         title='Iniciar nova tarefa'
+                        key='botao_submit'
                     />
-                ) : (
+                )}
+
+                {!!state.activeTask && (
                     <DefaultButton 
                         type='button' 
                         icon={<StopCircleIcon />} 
                         color='red'
                         aria-label='Interromper tarefa atual'
                         title='Interromper tarefa atual'
+                        onClick={handleInterruptTask}
+                        key='botao_button'
                     />
                 )}
                 
